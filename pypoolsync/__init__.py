30f4d901991a7bab0eb7da7ec3aa20372d9c0b8e
John Gemkow <jgemkow94@gmail.com>
2025-02-27 10:09:24 -0600

    Bump version

--- pypoolsync/__init__.py
+++ pypoolsync/__init__.py
@@ -3,4 +3,4 @@ from .exceptions import PoolsyncApiException, PoolsyncAuthenticationError
 from .poolsync import Poolsync, PoolsyncDevice, PoolSyncChlorsyncSWG
 
 __all__ = ["Poolsync", "PoolsyncApiException", "PoolsyncAuthenticationError", "PoolsyncDevice", "PoolSyncChlorsyncSWG"]
-__version__ = "0.1.3"
+__version__ = "0.1.4"
